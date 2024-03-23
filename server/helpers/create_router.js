bbcbb35e76de6d6f9c7167005ef9d1acb024f5ba
Jemma Phillips <jemma_phillips1@hotmail.co.uk>
2024-03-23 17:19:39 +0000

    add react boiler plate and build out server w/ crud actions to mongodb

---
+++ server/helpers/create_router.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const { ObjectId } = require("mongodb");
+
+const createRouter = function (collection) {
+  const router = express.Router();
+
+  // SHOW ALL
+  router.get("/", (req, res) => {
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ status: 500, error: err });
+      });
+  });
+
+  // SHOW BY ID
+  router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: new ObjectId(id) })
+      .then((doc) => res.json(doc))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ status: 500, error: err });
+      });
+  });
+
+  // CREATE
+  router.post("/", (req, res) => {
+    const newData = req.body;
+    collection
+      .insertOne(newData)
+      .then((result) => {
+        res.json({
+          _id: result.insertedId,
+          ...newData,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ status: 500, error: err });
+      });
+  });
+
+  // UPDATE
+  router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    collection
+      .updateOne({ _id: new ObjectId(id) }, { $set: updatedData })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ status: 500, error: err });
+      });
+  });
+
+  // DELETE - by id i.e one weather item 
+  router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    collection
+      .deleteOne({ _id: new ObjectId(id) })
+      .then((result) => {
+        if (result.deletedCount === 0) {
+          return res.status(404).json({ error: "Not Found" });
+        }
+        res.status(200).json({ message: "Successfully deleted" });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json({ status: 500, error: err });
+      });
+  });
+
+  return router;
+};
+
+module.exports = createRouter;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,31 @@
 const express = require('express');
 const app = express();
 
-// express server here
+// require parser to deal with the body object during weather post request
+const parser = require('body-parser');
+
+// enable cors
+const cors = require('cors');
+app.use(cors());
+
+// use parser
+app.use(parser.json());
+
+// mongodb is used for the weathersaves collection 
+// using mongodb compass can view db which will save api data to be persisted
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('weathersavesDB');
+    const weathersavesCollection = db.collection('weathersaves');
+    const weathersavesRouter = createRouter(weathersavesCollection);
+    app.use('/api/weathersaves', weathersavesRouter);
+  })
+  .catch(console.err);
+
+app.listen(3000, function () {
+  console.log(`Listening on port ${ this.address().port }`);
+});
